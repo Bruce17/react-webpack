b8f36fc4b0dd472d86f8af36dfc961212db593c3
Michael <Bruce17@users.noreply.github.com>
2016-04-29 10:25:19 +0200

    better :wink:

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var path             = require('path');
 
 var appName = 'app';
 var host    = process.env.IP || '0.0.0.0';
-var port    = process.env.PORT || '9000';
+var port    = process.env.PORT || '1337';
 
 var plugins = [], outputFile;
 
